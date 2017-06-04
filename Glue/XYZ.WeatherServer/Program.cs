c1509a6d9443a53ebe3dcda4851a30d56f6ab9f1
Dmitriy@ITAdapter <dmitriy@itadapter.com>
2017-06-04 17:09:41 -0400

    add server

--- Glue/XYZ.WeatherServer/Program.cs
+++ Glue/XYZ.WeatherServer/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using NFX;
+using NFX.ApplicationModel;
 
 namespace XYZ.WeatherServer
 {
+
+  //Run this program from command line.
+  // You can also experiment with different configuration files which can be injected using `-config` switch:
+  //
+  // This will use the default config names as EXE entry point
+  //  c:\> XYZ.WeatherServer
+  //
+  // This will use a different config file
+  //  c:\> XYZ.WeatherServer  -config "mytest.laconf"
+  //
   class Program
   {
     static void Main(string[] args)
     {
+      try
+      {
+        //At first, we setup an app container.
+        //Glue is built-in app container, so it will automatically inject dependencies and 
+        //start the server hosting as listed in config;
+        //Passing null to `rootConfig` - explicitly tells container to use the config file co-located with an EXE
+        //we could have written config right here, or we could have built service host by hand allocating classes
+        using (new ServiceBaseApplication(args, rootConfig: null))
+        {
+          Console.WriteLine("Server is running, endpoints:");
+          Console.WriteLine("-----------------------------");
+   
+          //Loop through all endpoints served by this application
+          foreach (var service in App.Glue.Servers)
+            Console.WriteLine("    " + service);
+
+          Console.WriteLine("Hit <enter> to exit");
+          Console.ReadLine();
+        }
+      }
+      catch (Exception ex)//we get here in case of error in app config file
+      {
+        Console.WriteLine("Exception leaked (most likely bad app config):");
+
+        //In NFX exception types represents the component area of the application
+        //it is usually enough to know what went wrong just by seeing exception type
+        //without looking at stack trace at all
+        Console.WriteLine(ex.ToMessageWithType());
+        Environment.ExitCode = -1;
+      }
     }
   }
 }
--- Glue/XYZ.WeatherServer/Weather.cs
+++ Glue/XYZ.WeatherServer/Weather.cs
@@ -4,23 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NFX;
+using NFX.Parsing;
+
 using XYZ.BusinessDomain.Contracts;
 
 namespace XYZ.WeatherServer
 {
   /// <summary>
-  /// Implements XYZ.BusinessDomain.Contracts.IWeather service contract
+  /// Implements XYZ.BusinessDomain.Contracts.IWeather service contract.
+  /// The IWeather service in Singleton by default, so this class instance will be created only once
   /// </summary>
   public class Weather : IWeather
   {
     public WeatherDay GetTodaysWheather(string area)
     {
-      throw new NotImplementedException();
+      return makeFake(App.TimeSource.UTCNow, area);
     }
-
+    
     public WeatherDay[] GetWheatherForecast(string area, DateTime start, int days)
     {
-      throw new NotImplementedException();
+      var result = new WeatherDay[days];
+      for (var i = 0; i < result.Length; i++)
+        result[i] = makeFake(start.AddDays(i), area);
+
+      return result;
+    }
+
+    //makes fake weather data for the day
+    private WeatherDay makeFake(DateTime when, string area)
+    {
+      return new WeatherDay
+      {
+        AsOfDate = when,
+        LocalityName = "{0} near {1}".Args(area, NaturalTextGenerator.GenerateCityName()),
+        PrecipitationChance = ExternalRandomGenerator.Instance.NextScaledRandomInteger(0,100),
+        TemperatureLowF = ExternalRandomGenerator.Instance.NextScaledRandomInteger(42, 50),
+        TemperatureHighF = ExternalRandomGenerator.Instance.NextScaledRandomInteger(62, 74),
+      };
     }
   }
 }
